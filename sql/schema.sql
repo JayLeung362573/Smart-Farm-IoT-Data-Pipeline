c553859d109bea32a5c2e7b0fbb95f72dd6019d0
Jiawei Liang <jla1028@sfu.ca>
2026-03-02 13:22:37 -0800

    Complete IoT data pipeline: multithreaded simulator and postgres ingestion

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,3 +1,9 @@
 CREATE_TABLE sensor_data(
-    
-)
+    id SERIAL PRIMARY KEY,
+    sensor_id INT NOT NULL,
+    moisture FLOAT NOT NULL,
+    temperature FLOAT NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+CREATE INDEX idx_sensor_id ON sensor_data(sensor_id);
